248963d9f4607fcca58f3585b4ef321778df94b4
helei.sig11 <helei.sig11@bytedance.com>
2021-06-10 15:20:00 +0800

    nvmf: support hdgst and ddgst for transport-tcp
    Signed-off-by: helei.sig11 <helei.sig11@bytedance.com>

--- lib/log.c
+++ lib/log.c
@@ -6,7 +6,6 @@
 
 #include "log.h"
 
-#include <assert.h>
 #include <stdarg.h>
 
 #include "nvmf-private.h"
@@ -20,20 +19,6 @@ void nvmf_default_log_fn(int log_level, const char *message)
 const char *log_level_tag[] = {"ERROR", "WARN", "DEBUG"};
 __thread char thread_name_buff[17] = {0};
 __thread const char *thread_name = NULL;
-
-void nvmf_options_set_log_level(nvmf_options_t opts, int log_level)
-{
-    assert(log_level >= LOG_LEVEL_ERROR);
-    assert(log_level <= LOG_LEVEL_DEBUG);
-    struct nvmf_ctrl_options *__opts = (struct nvmf_ctrl_options *)opts;
-    __opts->log_level = log_level;
-}
-
-void nvmf_options_set_log_fn(nvmf_options_t opts, nvmf_log_fn fn)
-{
-    struct nvmf_ctrl_options *__opts = (struct nvmf_ctrl_options *)opts;
-    __opts->log_fn = fn;
-}
 
 void nvmf_log_message(nvmf_ctrl_t ctrl, int log_level, const char *fmt, ...)
 {
